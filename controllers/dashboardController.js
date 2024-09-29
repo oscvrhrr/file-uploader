6263b2671c261d96102ceb86e9da33e01cace4ad
Oscar Herrera <a.oscarherrera@gmail.com>
2024-09-28 20:36:05 -0500

    installed supabase, setup supabase client, user can upload files to storage

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,12 +1,14 @@
 const db = require("../db/queries")
+const supabase = require("../storage/supabaseClient")
 
 
-async function  getUserAndData (req, res) {
+async function getUserAndData (req, res) {
   try {
     const user = await req.user;
     const drive = await db.readQueries.getDrive(user.id);
-    const folders = await db.readQueries.getFolders(drive.id)
-    res.status(200).render("dashboard", { user: user, drive: drive, folders: folders });
+    const folders = await db.readQueries.getFolders(drive.id);
+    const files = await db.readQueries.getFiles(drive.id);
+    res.status(200).render("dashboard", { user: user, drive: drive, folders: folders, files: files });
   } catch (err) {
     console.error(err);
   }
@@ -19,7 +21,8 @@ async function createFolderInDrive(req, res) {
     const drive = await db.readQueries.getDrive(req.user.id);
     await db.createQueries.createFolder(name, drive.id);
     const folders = await db.readQueries.getFolders(drive.id);
-    res.status(201).render("dashboard", { user: req.user, drive: drive, folders: folders});
+    const files = await db.readQueries.getFiles(drive.id);
+    res.status(201).render("dashboard", { user: req.user, folders: folders, files: files});
   } catch (error) {
     console.error("Internal server error");
   }
@@ -28,7 +31,11 @@ async function createFolderInDrive(req, res) {
 async function uploadFileInDrive(req, res) {
   try {
     const { body, file } = req;
-    res.status(200).render("dashboard", { user: req.user }, console.log(body, file));
+    await supabase.storage.from('Files_fileupload').upload(file.path, file)
+    const drive = await db.readQueries.getDrive(req.user.id);
+    const folders = await db.readQueries.getFolders(drive.id);
+    const files = await db.readQueries.getFiles(drive.id);
+    res.status(200).render("dashboard", { user: req.user, folders: folders, files: files }, console.log(body, file));
   } catch (err) {
     console.error(err)
   }
