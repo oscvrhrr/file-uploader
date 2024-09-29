6e316aee4580e6288ff2a2e651b3ccf8c97fed44
Oscar Herrera <a.oscarherrera@gmail.com>
2024-09-29 10:56:27 -0500

    simultaneously creates new user and drive

--- db/queries.js
+++ db/queries.js
@@ -11,7 +11,7 @@ const createQueries = {
                 password,
                 drives: {
                      create: {
-                        name
+                        
                      }
                 }
             }
