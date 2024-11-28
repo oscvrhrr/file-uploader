a4fc9453e2eeb40b0de61aea199f6ed9595ae753
Oscar Herrera <a.oscarherrera@gmail.com>
2024-11-28 15:31:09 -0600

    implemented jwt stategy

--- server/auth/passportConfig.js
+++ server/auth/passportConfig.js
@@ -1,43 +1,52 @@
 const passport = require("passport")
 const bcrypt = require("bcryptjs")
 const LocalStrategy = require("passport-local");
+const JwtStrategy = require("passport-jwt").Strategy
+const ExtJwt = require("passport-jwt").ExtractJwt
 const { prisma } = require("../db/queries")
 
+opts = {
+ jwtFromRequest: ExtJwt.fromAuthHeaderAsBearerToken(),
+ secretOrKey: process.env.JWT_SECRET
+}
+
+
 passport.use(
-    new LocalStrategy(async (username, password, done) => {
-        try {
-            const user = await prisma.user.findFirst({
-                where: { username },
-            })
-            if (!user) {
-                return done(null, false, { message: "Incorrect Username"});
-            }
-            const match = await bcrypt.compare(password, user.password)
-            if (!match) {
-                return done(null, false, { message: "Incorrect Password"});
-            }
-            return done(null, user)
-        } catch (err) {
-            return done(err)
+  new LocalStrategy(async (username, password, done) => {
+    try {
+        const user = await prisma.user.findFirst({
+          where: { username },
+        })
+        if (!user) {
+          return done(null, false, { message: "Incorrect Username"});
         }
-    })
+        const match = await bcrypt.compare(password, user.password)
+        if (!match) {
+          return done(null, false, { message: "Incorrect Password"});
+        }
+        return done(null, user)
+    } catch (err) {
+      return done(err)
+    }
+  })
 );
 
-passport.serializeUser((user, done) => {
-    done(null, user.id)
-});
+passport.use(new JwtStrategy(opts, async function(jwt_payload, done) {
+  try {
+    const user = await prisma.findFirst({
+      where: { id: jwt_payload.id }
+    });
 
-passport.deserializeUser( async(id, done) => {
-    try {
-        const user = prisma.user.findFirst({
-            where: { id },
-        });
-        
-        done(null, user)
-    } catch(err) {
-        return done(err)
+    if(user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
     }
-});
+  } catch (error) {
+    return done(error, false)
+  }
+}));
+
 
 
 module.exports = passport
