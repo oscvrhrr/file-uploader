521284ee24f510b66c5c70d8c5801e5f2f026ed0
Oscar Herrera <a.oscarherrera@gmail.com>
2024-12-10 11:15:28 -0600

    new feature to upload file

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,19 +1,28 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import Navbar from "../components/Navbar"
-import Drive from "../components/Drive"
-import { DriveType } from "../types/drive"
-import { Link } from "react-router"
-import { useEffect, useContext, useState } from "react"
-import { UserContext } from "../components/context/UserContext"
+import Navbar from "../components/Navbar";
+import Drive from "../components/Drive";
+import UploadFile from "../components/UploadFile";
+import { DriveType } from "../types/drive";
+import { Link } from "react-router";
+import { useEffect, useContext, useState } from "react";
+import { UserContext } from "../components/context/UserContext";
+import { Button, Container } from "@radix-ui/themes";
+import { UploadIcon, PlusIcon } from "@radix-ui/react-icons";
 
 
+const Dashboard = () => {
+  const { setUser } = useContext(UserContext);
+  const [toggle, setToggle] = useState(false)
+  const [drive, setDrive] = useState<DriveType>({
+    id: null,
+    ownerId: null,
+    folders: null,
+    files: null,
+  });
 
-
-
-const Dashboard = () => { 
- const { setUser } = useContext(UserContext)
- const [drive, setDrive] = useState<DriveType>({id: null, ownerId: null, folders: null, files: null})
-
+  const handleButtonToggle = () => {
+    setToggle(!toggle)
+  }
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -24,35 +33,49 @@ const Dashboard = () => {
           mode: "cors",
           headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-          }
+            "Authorization": `Bearer ${token}`,
+          },
         });
-        if(response.ok) {
+        if (response.ok) {
           const parsedData = await response.json();
-          console.log(parsedData)
-          setUser(parsedData.user)
-          setDrive(parsedData.drive)
+          console.log(parsedData);
+          setUser(parsedData.user);
+          setDrive(parsedData.drive);
         }
-      } catch(error) {
-        console.log(error, "error fetching user")
+      } catch (error) {
+        console.log(error, "error fetching user");
       }
-    }
-    fetchUser()
-  }, [])
-
+    };
+    fetchUser();
+  }, []);
 
   return (
     <div className="bg-radixindigo-200 h-screen">
-
       <Navbar>
-        <Link className="hover:bg-radixgray-300 rounded-lg px-1 py-0.5 cursor-pointer" to="/" >Logout</Link>
+        <Link
+          className="hover:bg-radixgray-300 rounded-lg px-1 py-0.5 cursor-pointer"
+          to="/"
+        >
+          Logout
+        </Link>
       </Navbar>
-      <div className="mx-10 my-4">
-        
-      </div>
-      <Drive drive={drive}/>
-    </div>
-  )
-}
+      <Container className="flex justify-end px-10 py-4 ">
+        <div className="flex">
+          <Button className="flex text-sm text-white items-center bg-radixindigo-900 hover:bg-radixindigo-1000 px-2 py-1 rounded" size="2" variant="soft" radius="large">
+            <PlusIcon/> Create folder
+          </Button>
+          { toggle && 
+            <UploadFile toggle={ handleButtonToggle }/>
 
-export default Dashboard
+          }
+          <Button onClick={handleButtonToggle} className="flex text-sm text-white items-center bg-radixindigo-900 hover:bg-radixindigo-1000 px-2 py-1 ml-4 rounded" size="3" variant="soft" radius="large">
+            <UploadIcon/> Upload file
+          </Button>
+        </div>
+      </Container>
+      <Drive drive={drive} />
+    </div>
+  );
+};
+
+export default Dashboard;
