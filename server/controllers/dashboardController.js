8916bb000eaf1063f76bdf749cc1c635fb1244b3
Oscar Herrera <a.oscarherrera@gmail.com>
2024-12-10 08:33:36 -0600

    removed functions

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,47 +1,8 @@
 const db = require("../db/queries")
-const decodeFile = require("../utils/decodeFile")
-const uploadFileInBucket = require("../storage/uploadFile");
 
 
 
 
-async function getUserAndDrive (req, res) {
-  try {
-    const user = await req.user;
-    const drive = await db.readQueries.getDrive(user.id);
-    res.status(200).render("dashboard", { user: user, drive: drive, folders: drive.folders, files: drive.files  });
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-
-async function createFolderInDrive(req, res) {
-  try {
-    const { name } = req.body;
-    const user = await req.user;
-    const drive = await db.readQueries.getDrive(user.id);
-    await db.createQueries.createFolder(name, drive.id);
-    res.status(201).redirect("/dashboard")
-  } catch (error) {
-    console.error("Internal server error");
-  }
-}
-
-async function uploadFileInDrive(req, res) {
-  try {
-    const { body, file } = req;
-    const user = await req.user;
-    const metaFile = file;
-    const outputFile = decodeFile(metaFile);
-    uploadFileInBucket(outputFile, metaFile, user.username);
-    const drive = await db.readQueries.getDrive(user.id);
-    await db.createQueries.createFile(body['file-name'], metaFile.size, `user-uploads/${user.username}/${metaFile.originalname}`, drive.id, null)
-    res.status(200).redirect("/dashboard")
-  } catch (err) {
-    console.error(err)
-  }
-}
 
 async function getFileData(req, res) {
   try {
@@ -74,19 +35,10 @@ async function deleteFolderById(req, res) {
 }
 
 
-async function logOutOfSession(req, res, next) {
-  req.logOut((err) => err ? next(err) : res.redirect("/"));
-}
-
-
 
 
 module.exports = {
-    getUserAndDrive,
-    createFolderInDrive,
-    uploadFileInDrive,
     getFileData,
     deleteFileById,
     deleteFolderById,
-    logOutOfSession
 }
