a9a93a07808aabeb5ba00a3659d4f545a345481c
Oscar Herrera <a.oscarherrera@gmail.com>
2024-12-10 11:11:54 -0600

    new buttons on nav

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,42 +1,48 @@
-import Navbar from "../components/Navbar"
-import Hero from "../components/Hero"
-import LoginForm from "../components/LoginForm"
-import SignupForm from "../components/SignupForm"
-import { useState } from "react"
-
-
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import LoginForm from "../components/LoginForm";
+import SignupForm from "../components/SignupForm";
+import { useState } from "react";
 
 const Landing = () => {
-  const [toggleLogin, setToggleLogin] = useState(false)
-  const [toggleSignUp, setToggleSignUp] = useState(false)
-  
+  const [toggleLogin, setToggleLogin] = useState(false);
+  const [toggleSignUp, setToggleSignUp] = useState(false);
+
   const handleToggleLogin = () => {
-    setToggleLogin(!toggleLogin)
-  }
+    setToggleLogin(!toggleLogin);
+  };
 
   const handleToggleSignUp = () => {
-    setToggleSignUp(!toggleSignUp)
-  }
-
-
+    setToggleSignUp(!toggleSignUp);
+  };
 
   return (
     <>
       <div className="bg-radixindigo-200 h-screen">
-        <Navbar >
-            <p className="hover:bg-radixgray-300 rounded-lg px-1 py-0.5 cursor-pointer">Home</p>
-            <p className=" hover:bg-radixgray-300 rounded-lg px-1 py-0.5 cursor-pointer" onClick={ handleToggleLogin }>Log in</p>
-            <p className=" hover:bg-radixgray-300 rounded-lg px-1 py-0.5 cursor-pointer" onClick={ handleToggleSignUp }>Sign up</p> 
-        {toggleLogin ? <LoginForm/> : null }
-        {toggleSignUp ? <SignupForm/> : null }
+        <Navbar>
+          <p className="hover:bg-radixgray-300 rounded-lg px-1 py-0.5 cursor-pointer">
+            Home
+          </p>
+          <p
+            className=" hover:bg-radixgray-300 rounded-lg px-1 py-0.5 cursor-pointer"
+            onClick={handleToggleLogin}
+          >
+            Log in
+          </p>
+          <p
+            className=" hover:bg-radixgray-300 rounded-lg px-1 py-0.5 cursor-pointer"
+            onClick={handleToggleSignUp}
+          >
+            Sign up
+          </p>
+          {toggleLogin ? <LoginForm /> : null}
+          {toggleSignUp ? <SignupForm /> : null}
         </Navbar>
-        
-        <Hero/>
 
+        <Hero />
       </div>
-
     </>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
