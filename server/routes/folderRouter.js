a1546671d70b1fc8003c4380ad2ca5408e75a2c7
Oscar Herrera <a.oscarherrera@gmail.com>
2024-12-12 09:40:15 -0600

    cleaned up folder routes

--- server/routes/folderRouter.js
+++ server/routes/folderRouter.js
@@ -1,35 +1,35 @@
 const Router = require("express");
-const multer = require("multer");
-
-
+// const multer = require("multer");
 
 
 const folderRouter = Router();
-const { createFolderInDrive, uploadFileInDrive, getFileData, deleteFileById, deleteFolderById } = require("../controllers/dashboardController")
-const { getFolderAndFilesById, uploadFileInFolder } = require("../controllers/folderController")
-
-const storage = multer.memoryStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads/')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    }
-});
-
-const upload = multer({ storage });
 
 
+// const { deleteFileById, deleteFolderById } = require("../controllers/dashboardController")
+const { getFolderAndFilesById, getFileData, uploadFileInFolder } = require("../controllers/folderController")
 
-folderRouter.post("/file/:fileId", deleteFileById )
+// const storage = multer.memoryStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, './uploads/')
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, file.originalname)
+//     }
+// });
+
+// const upload = multer({ storage });
+
+
+
+// folderRouter.post("/file/:fileId", deleteFileById )
 
 folderRouter.get("/folder/:folderId", getFolderAndFilesById)
 
-folderRouter.post("/folder/:folderId/file", upload.single("file"), uploadFileInFolder);
+// folderRouter.post("/folder/:folderId/file", upload.single("file"), uploadFileInFolder);
 
 // folderRouter.post("/create-folder", createFolderInDrive);
 
-folderRouter.post("/folder/:folderId", deleteFolderById )
+// folderRouter.post("/folder/:folderId", deleteFolderById )
 
 folderRouter.get("/file/:fileId", getFileData);
 
