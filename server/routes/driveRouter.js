7fd045812c6e8a0523ff315fb5fc76ba4e5e6979
Oscar Herrera <a.oscarherrera@gmail.com>
2024-12-12 09:39:32 -0600

    configured multer & uploaded file into supabase storage

--- server/routes/driveRouter.js
+++ server/routes/driveRouter.js
@@ -1,18 +1,30 @@
 const Router = require("express");
 const driveController = require("../controllers/driveController")
+const multer = require("multer")
+
+const storage = multer.memoryStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+
+const upload = multer({ storage });
 
 
 const driveRouter = Router()
 
 //new folder in a drive
-driveRouter.post("/drives/:driveid/folder", driveController.createFolderInDrive)
+driveRouter.post("/:driveid/folder", driveController.createFolderInDrive)
 
 
 //new file in a drive
-driveRouter.post("/drives/:driveid/file", driveController.uploadFileInDrive)
+driveRouter.post("/:driveid/file", upload.single("file"), driveController.uploadFileInDrive)
 
 
 //get a drive
-driveRouter.get("/drives/:driveid")
+driveRouter.get("/:driveid")
 
 module.exports = driveRouter;
