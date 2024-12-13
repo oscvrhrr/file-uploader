c866f4198c904bea86194b130ab622579db15391
Oscar Herrera <a.oscarherrera@gmail.com>
2024-12-12 19:21:04 -0600

    included api url as env variable

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({origin: "http://localhost:3000"}));
+app.use(cors({ origin: process.env.BASE_URL }));
 
 
 
