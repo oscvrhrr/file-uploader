c21db9a5c3d1b835fa159b70250ff1bfddb2207a
Oscar Herrera <a.oscarherrera@gmail.com>
2024-12-12 20:03:08 -0600

    added production frontend to cors

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: process.env.BASE_URL }));
+app.use(cors({ origin: "https://file-uploader-beige.vercel.app/" }));
 
 
 
